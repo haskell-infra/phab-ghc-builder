67e17ddaf84971303a212c683ce53b3d708795e8
Austin Seipp <aseipp@pobox.com>
2014-07-11 23:06:27 -0500

    Misc fixes to builder.sh
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- builder.sh
+++ builder.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
-
-set -e
+#set -e
+unset CDPATH
 
 BUILDID=$1
 DIFF=$2
 REVISION=$3
 PHID=$4
+
+CONDUITURI=https://phabricator.haskell.org
 
 if [[ -z "$BUILDID" ]] || [[ -z "$DIFF" ]] || [[ -z "$REVISION" ]] || [[ -z "$PHID" ]]; then
   echo "err: usage - ./builder.sh <build-id> <diff-id> <revision> <phid>"
@@ -19,39 +21,40 @@ git clone --recursive git://git.haskell.org/ghc.git
 cd ghc
 
 # Apply patch, perform build
-#arc patch --nobranch --diff $DIFF
+arc patch --nobranch --diff $DIFF
 NUM_CPUS=3 # TODO FIXME
 CPUS=$NUM_CPUS ./validate 2>&1 > ./validate.log
 BUILDRES=$?
 
 ## -- Upload result file
-LOGS=`cat ./validate.log | base64`
-FILEMSG="{\"data_base64\":\"$LOGS\",\"name\":\"ghc-build-B$BUILDID-D$REVISION-d$DIFF.txt\"}"
+LOGS=`cat ./validate.log | gzip | base64`
+FILEMSG="{\"data_base64\":\"$LOGS\",\"name\":\"ghc-build-B$BUILDID-D$REVISION-d$DIFF.txt.gz\"}"
 # Get PHID from response
-PHID=`echo '$FILEMSG' | arc call-conduit file.upload | jq '.response'`
+PHID=`echo "$FILEMSG" | arc call-conduit file.upload --conduit-uri=$CONDUITURI | jq '.response'`
+
 FILEMSG2="{\"phid\":$PHID}"
 # Get File identifier
-FID=`echo '$FILEMSG2' | arc call-conduit file.info | jq '.response.id | tonumber'`
+FID=`echo "$FILEMSG2" | arc call-conduit file.info --conduit-uri=$CONDUITURI | jq '.response.id | tonumber'`
 FILEID="{F$FID}"
 
 # Post back to Harbormaster about the build status
 PASSMSG="{\"buildTargetPHID\":\"$PHID\",\"type\":\"pass\"}"
 FAILMSG="{\"buildTargetPHID\":\"$PHID\",\"type\":\"fail\"}"
 
-if [ "x$BUILDRES" = "x0" ];
-  echo "'$PASSMSG'" | arc call-conduit harbormaster.sendmessage
+if [ "x$BUILDRES" = "x0" ]; then
+  echo "'$PASSMSG'" #| arc call-conduit harbormaster.sendmessage --conduit-uri=$CONDUITURI
 else
-  echo "'$FAILMSG'" | arc call-conduit harbormaster.sendmessage
+  echo "'$FAILMSG'" #| arc call-conduit harbormaster.sendmessage --conduit-uri=$CONDUITURI
 fi
 
 # Post passing/failing comment on the revision.
-PASSMSG="Yay! Build B$BUILDID (D$REVISION, Diff $DIFF) has **succeeded**! Full logs available at {$FILEID}."
-FAILMSG="Whoops, Build B$BUILDID (D$REVISION, Diff $DIFF) has **failed**! Full logs available at {$FILEID}."
+PASSMSG="Yay! Build B$BUILDID (D$REVISION, Diff $DIFF) has **succeeded**! Full logs available at $FILEID."
+FAILMSG="Whoops, Build B$BUILDID (D$REVISION, Diff $DIFF) has **failed**! Full logs available at $FILEID."
 
-if [ "x$BUILDRES" = "x0" ];
-  echo "'{\"revision_id\":\"$REVISION\",\"message\":\"$PASSMSG\"}'" \
-    | arc call-conduit differential.createcomment
+if [ "x$BUILDRES" = "x0" ]; then
+  echo "'{\"revision_id\":\"$REVISION\",\"message\":\"$PASSMSG\"}'" #\
+    #| arc call-conduit differential.createcomment --conduit-uri=$CONDUITURI
 else
-  echo "'{\"revision_id\":\"$REVISION\",\"message\":\"$FAILMSG\"}'" \
-    | arc call-conduit differential.createcomment
+  echo "'{\"revision_id\":\"$REVISION\",\"message\":\"$FAILMSG\"}'" #\
+    #| arc call-conduit differential.createcomment --conduit-uri=$CONDUITURI
 fi
